e99bfffd763edc1165dd58e83dab36daf52a76ff
Femme-js <itsjeevu@gmail.com>
2023-03-04 11:07:05 +0530

    updates

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="auto_tabnet",
-    version = "0.1",
-    author="Ersilia",
-    author_email="hello@ersilia.io",
-    description="Auto Tabnet",
-    keywords="automl, hyperparameter optimisation, tabular",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ersilia-os/auto-tabnet",
-    project_urls={
-        "Documentation": "",
-        "Source Code": "https://github.com/ersilia-os/auto-tabnet",
-    },
-    package_dir={"": "auto_tabnet"},
-    packages=setuptools.find_packages(where="auto_tabnet"),
-    classifiers=[
-        'Programming Language :: Python :: 3 :: Only',
-    ],
-    python_requires=">=3.7",
+setup(
+    name = 'auto-tabnet',
+    packages = ['auto-tabnet'],
+    version = 'v1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Automated implementation of Google TabNet.',
+    author = 'Ersilia',
+    author_email = 'hello@ersilia.io',
+    url = 'https://github.com/ersilia-os/auto-tabnet',
+    download_url = 'https://github.com/ersilia-os/auto-tabnet/archive/refs/tags/v1.0.0.tar.gz',
+    keywords = ['automl', 'tabular'],
+    classifiers = [],
 )
